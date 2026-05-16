48b4e9e9c8b2f2f85838c7a7eb84536f49fe1e4c
Ivan <ivan@ya.ru>
2026-05-17 00:24:12 +0300

    Убрать tests/ из репозитория

--- tests/test_smoke.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import text
-
-
-def test_db_session_works(db_session):
-    """Фикстура отдаёт рабочую сессию к in-memory SQLite."""
-    result = db_session.execute(text("SELECT 1")).scalar()
-    assert result == 1
